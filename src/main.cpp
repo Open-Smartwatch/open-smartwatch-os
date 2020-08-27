5e80d15e040fb2be890b60be3c96b7f8c7c6c491
Paul Smith <code@uvwxy.de>
2020-08-27 22:30:56 +0200

    version: perlin world

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "../lib/p3dt-lib-arduino/src/gfx/p3dt_gfx_2d.h"
 #include "../lib/p3dt-lib-arduino/src/gfx/p3dt_gfx_util.h"
 #include "../lib/p3dt-lib-arduino/src/math/p3dt_math_angles.h"
+#include "../lib/p3dt-lib-arduino/src/math/perlin/SimplexNoise.h"
 #include "Arduino_Display.h"
 #include "Arduino_ESP32SPI.h"
 #include "Arduino_GFX.h"
@@ -22,41 +23,54 @@ Arduino_GC9A01 *gfx = new Arduino_GC9A01(bus, TFT_RST, 0 /* rotation */, true /*
 #define BUF_W 240
 #define BUF_H 240
 #define CHUNK_H 16  // = 15 vertical chunks
-#define FIRE_WIDTH 240
-#define FIRE_HEIGHT 80
-uint8_t **firePixels = new uint8_t *[FIRE_HEIGHT];
+
+SimplexNoise sn;
 
 Graphics2D gfx2d(BUF_W, BUF_H, CHUNK_H /*chunk size*/);
 
 uint8_t tipX = 0;
 uint8_t tipY = 0;
 
-TaskHandle_t Task1;
-void fireTask(void *pvParameters) {
-  while (true) {
-    calcFire(firePixels, BUF_W, FIRE_HEIGHT);
-    uint8_t fireOffset = (BUF_W - FIRE_HEIGHT);
-    if (tipY > fireOffset) {
-      firePixels[tipY - fireOffset][tipX - 1] = 30;
-      firePixels[tipY - fireOffset][tipX] = 34;
-      firePixels[tipY - fireOffset + 1][tipX] = 30;
-      firePixels[tipY - fireOffset][tipX + 1] = 30;
-    }
-    delay(10);
-  }
-  // doCalcFire = false;
-}
-
 void setup() {
   Serial.begin(115200);
   gfx->begin();
-  // setup array rows
-  for (int i = 0; i < FIRE_HEIGHT; i++) {
-    firePixels[i] = new uint8_t[FIRE_WIDTH];
-  }
-  setupFire(firePixels, FIRE_WIDTH, FIRE_HEIGHT);
-  xTaskCreatePinnedToCore(fireTask, "FireTask", 1000 /*stack*/, NULL /*input*/, 0 /*prio*/, &Task1 /*handle*/, 0);
 }
+int16_t mix(uint8_t base, float n1, float n2) { return (n1 - abs(n2)) * base; }
+
+void fillPerlin(uint16_t counter) {
+  for (uint8_t x = 0; x < BUF_W; x++) {
+    for (uint8_t y = 0; y < BUF_H; y++) {
+      float n1 = sn.fractal(7, (x + counter) / 120.0, y / 120.0);  // sn.noise returns [-1,1]
+      float n2 = sn.noise((x + counter) / 2, y / 2);               // sn.noise returns [-1,1]
+
+      uint8_t r = n1 > 0 ? (n1 - n2) * 255 : 0;
+      uint8_t g = r;
+      uint8_t b = r;
+
+      uint16_t color = rgb565(r, g, b);
+
+      if (n1 < -.1) {  // deep water
+        color = rgb565(72 * (1 + n1) + mix(5, n1, n2), 72 * (1 + n1) + mix(5, n1, n2), 190 * (1 + n1) + mix(5, n1, n2));
+      } else if (n1 < 0) {  // shallow water plateaus
+        color = rgb565(72 + mix(5, n1, n2), 72 + mix(5, n1, n2), 190 + mix(5, n1, n2));
+      } else if (n1 < .0125) {  // beaches
+        color = rgb565(200 + mix(5, n1, n2), 200 + mix(20, n1, n2), 47 + mix(5, n1, n2));
+      } else if (n1 < .2) {  // meadows
+        color = rgb565(85 + mix(40, n1, n2), 107 + mix(20, n1, n2), 47 + mix(20, n1, n2));
+      } else if (n1 < .7) {  // forest
+        color = rgb565((85 * (1 - (n1 - .2) * 2)) + (35 * ((n1 - .2) * 2)) - mix(10, n1, n2),
+                       (107 * (1 - (n1 - .2) * 2)) + (57 * ((n1 - .2) * 2)) - mix(20, n1, n2),
+                       (47 * (1 - (n1 - .2) * 2)) + (7 * ((n1 - .2) * 2)) - mix(10, n1, n2));
+      } else {
+        color = rgb565(35, 57, 7) - rgb565(85 + mix(40, n1, n2), 107 + mix(20, n1, n2), 47 + mix(20, n1, n2));
+      }
+
+      gfx2d.drawPixel(x, y, color);
+    }
+  }
+}
+
+void movePerlin() {}
 
 void loop() {
   static bool drawOnce = true;
@@ -66,22 +80,22 @@ void loop() {
 
   if (drawOnce) {
     drawOnce = false;
-    gfx->fillRect(0, 0, 240, 240, BLACK);
-    Serial.print("Setup running on core ");
-    Serial.println(xPortGetCoreID());
   }
 
   uint8_t cx = 119;
   uint8_t cy = 119;
   gfx2d.fill(rgb565(0, 0, 0));
 
-  mapFire(firePixels, FIRE_WIDTH, FIRE_HEIGHT, &gfx2d, 0, (BUF_H - FIRE_HEIGHT));
+  static uint16_t counter = 0;
+  counter++;
+
+  fillPerlin(counter);
   gfx2d.drawWatchFace(BUF_W / 2, BUF_H / 2, 119, rgb565(255, 255, 255));
   // draw handles
   // hour
   gfx2d.drawLine(cx, cy, rpx(cx, 33, h2d(seconds)), rpy(cy, 33, h2d(seconds)), WHITE);
   // // minute
-  gfx2d.drawLine(cx, cy, rpx(cx, 66, m2d(seconds)), rpy(cy, 66, m2d(seconds)), BLUE);
+  gfx2d.drawLine(cx, cy, rpx(cx, 66, m2d(seconds)), rpy(cy, 66, m2d(seconds)), WHITE);
   // // second
   gfx2d.drawLine(cx, cy, rpx(cx, 15, s2d(seconds) + 180), rpy(cy, 15, s2d(seconds) + 180), RED);  // short backwards
   tipX = rpx(cx, 90, s2d(seconds));
