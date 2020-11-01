ea83485365f90de6b6fdce129dc71bdcba22a4a8
Paul Smith <code@uvwxy.de>
2020-11-01 22:35:29 +0100

    add rotate + advanced rotate

--- math_angles.cpp
+++ math_angles.cpp
@@ -32,3 +32,21 @@ float h2d(long seconds) {
   long fd = (seconds / 3600) / 24;  // full days
   return ((seconds - fd * 24 * 3600) / 3600.0) * 30;
 }
+
+float sign(float x1, float y1, float x2, float y2, float x3, float y3) {
+  return (x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3);
+}
+
+bool pointInsideTriangle(float px, float py, float x1, float y1, float x2, float y2, float x3, float y3) {
+  float d1, d2, d3;
+  bool has_neg, has_pos;
+
+  d1 = sign(px, py, x1, y1, x2, y2);
+  d2 = sign(px, py, x2, y2, x3, y3);
+  d3 = sign(px, py, x3, y3, x1, y1);
+
+  has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+  has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+  return !(has_neg && has_pos);
+}
