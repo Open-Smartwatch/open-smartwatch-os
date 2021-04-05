53ff0bba507dc1abf10d79e8094e4d3c1dbac4c2
Samuel Kelemen <Samuel@Kelemen.us>
2021-04-05 20:44:23 +0200

    fix spelling: defaut -> default

--- src/osw_ui_util.cpp
+++ src/osw_ui_util.cpp
@@ -7,9 +7,9 @@ void print2Digits(OswHal* hal, long n) {
   hal->getCanvas()->print(n);
 }
 
-uint16_t defaultFontXOffset(uint16_t numChars, uint16_t scale) {  // works with defaut font only
+uint16_t defaultFontXOffset(uint16_t numChars, uint16_t scale) {  // works with default font only
   return numChars * 6 * scale;
 }
-uint16_t defaultFontYOffset(uint16_t numRows, uint16_t scale) {  // works with defaut font only
+uint16_t defaultFontYOffset(uint16_t numRows, uint16_t scale) {  // works with default font only
   return numRows * 8 * scale;
 }
