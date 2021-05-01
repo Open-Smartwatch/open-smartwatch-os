f5cadbe098f2b437301376ea15cc3591084e67db
Simon Beginn <simon@simonmicro.de>
2021-05-01 21:43:54 +0200

    Created header files for language support

---
+++ include/locales/de-DE.h
@@ -0,0 +1,12 @@
+#ifndef LANG_DE_H
+#define LANG_DE_H
+
+// General terms
+#define LANG_CONNECT "Verbinden"
+#define LANG_DISCONNECT "Trennen"
+
+// App: Time from web
+#define LANG_TFW_UPDATE "Abfragen"
+
+
+#endif
---
+++ include/locales/en-US.h
@@ -0,0 +1,11 @@
+#ifndef LANG_EN_H
+#define LANG_EN_H
+
+// General terms
+#define LANG_CONNECT "Connect"
+#define LANG_DISCONNECT "Disconnect"
+
+// App: Time from web
+#define LANG_TFW_UPDATE "Update"
+
+#endif
