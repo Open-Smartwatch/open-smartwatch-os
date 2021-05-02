47cb3b233a0670ecbeea8c5357366076245635bd
Simon Beginn <simon@simonmicro.de>
2021-05-03 00:24:51 +0200

    Added write test

--- src/apps/tools/config_mgmt.cpp
+++ src/apps/tools/config_mgmt.cpp
@@ -15,7 +15,14 @@ void OswAppConfigMgmt::loop(OswHal* hal) {
     hal->getCanvas()->setTextColor(rgb565(255, 255, 255));
     hal->getCanvas()->setTextSize(2);
 
-    if (hal->btn2Down()) {
+    if (hal->btn3Down()) {
+        OswConfig::getInstance()->enableWrite();
+        OswConfig::getInstance()->putShort(OSW_CONFIG_WTF_COLOR_R, 20);
+        OswConfig::getInstance()->putShort(OSW_CONFIG_WTF_COLOR_G, 220);
+        OswConfig::getInstance()->putShort(OSW_CONFIG_WTF_COLOR_B, 40);
+        ESP.restart();
+        hal->clearBtn2();
+    } else if (hal->btn2Down()) {
         OswConfig::getInstance()->reset();
         ESP.restart();
         hal->clearBtn2();
@@ -23,8 +30,10 @@ void OswAppConfigMgmt::loop(OswHal* hal) {
 
     hal->getCanvas()->setCursor(20, 110);
     hal->getCanvas()->setTextSize(3);
-    hal->getCanvas()->print("Config mgmt");
+    hal->getCanvas()->print("Config tests");
     hal->getCanvas()->setTextSize(2);
+    hal->getCanvas()->setCursor(40, 50);
+    hal->getCanvas()->print("Test write...");
     hal->getCanvas()->setCursor(40, 190);
     hal->getCanvas()->print("Reset & Reload");
     hal->requestFlush();
