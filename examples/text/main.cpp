f786d509defda15168a7a5f91a3dc1e111afd497
Paul Smith <code@uvwxy.de>
2021-05-02 14:31:07 +0200

    cleanup text printing

--- examples/text/main.cpp
+++ examples/text/main.cpp
@@ -30,26 +30,42 @@ class RotationExampleWindow : public SDLWindowRGB565 {
     counter++;
     gfx->fill(rgb565(0, 0, 0));
 
+    gfx->setTextSize(1);
+    gfx->setTextLeftAligned();
+    gfx->setTextCursor(120, 20);
+    gfx->print("Left\naligned");
+    gfx->setTextRightAligned();
+    gfx->setTextCursor(120, 40);
+    gfx->print("Right\naligned");
+
+    gfx->setTextCursor(0, 120);
+    gfx->setTextLeftAligned();
+    gfx->setTextMiddleAligned();
+    gfx->print("middle1\nmiddle2");
+
+    gfx->setTextCenterAligned();
+    gfx->setTextTopAligned();
+    gfx->setTextCursor(120, 120);
+    gfx->print("Top aligned\n(top supports multiple_rows)");
+
+    gfx->setTextCenterAligned();
+    gfx->setTextBottomAligned();
+    gfx->setTextCursor(120, 120);
+    gfx->print("Bottom aligned");  // does not support multiple rows
+
+    gfx->setTextLeftAligned();
     gfx->setTextSize(2);
-    gfx->setTextCursor(220 - gfx->textCharXOffset(5), 42);
-    gfx->print("Start");
-    gfx->setTextCursor(220 - gfx->textCharXOffset(4), 182);
-    gfx->print("Stop");
-
-    gfx->setTextSize(4);
-    gfx->setTextCursor(gfx->textCharXOffset(1), 120 - gfx->textCharYOffset(1) / 2);
-    gfx->writeDigits(0, 1);
+    gfx->setTextCursor(120, 200);
+    // TODO: fix text wrapping on single character prints
+    // gfx->setTextWrap(false);;
+    gfx->setTextCursor(120 - gfx->getTextOfsetColumns(6.5), 210);
+    gfx->printDecimal(8, 2);
     gfx->print(":");
-    gfx->writeDigits(11, 2);
+    gfx->printDecimal(15, 2);
     gfx->print(":");
-    gfx->writeDigits(7, 3);
-
-    // Draw watch outline
-    gfx->drawCircle(119, 119, 119, rgb565(255, 255, 255));
-    gfx->fillFrame(216, 40, 10, 10, rgb565(200, 200, 200));
-    gfx->fillFrame(216, 190, 10, 10, rgb565(200, 200, 200));
-    gfx->fillFrame(13, 40, 10, 10, rgb565(200, 200, 200));
-    gfx->fillFrame(13, 190, 10, 10, rgb565(200, 200, 200));
+    gfx->printDecimal(47, 2);
+    gfx->print(".");
+    gfx->printDecimal(11, 4);
 
     delay(1000 / 30);
   }
