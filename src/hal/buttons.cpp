0dd7d9c3d78bbdc9c568283e5b407add64618c48
Paul Smith <code@uvwxy.de>
2021-05-08 09:25:07 +0200

    format buttons.cpp

--- src/hal/buttons.cpp
+++ src/hal/buttons.cpp
@@ -3,7 +3,6 @@
 #include "osw_hal.h"
 #include "osw_pins.h"
 
-
 #define DEFAULTLAUNCHER_LONG_PRESS 1000
 
 // Graphics2D screenBuffer(DISP_W, DISP_H, DISP_CHUNK_H);
@@ -16,7 +15,7 @@ void OswHal::setupButtons(void) {
   pinMode(BTN_2, INPUT);
   pinMode(BTN_3, INPUT);
 
-  //Buttons 
+  // Buttons
   long now = millis();
   _btn1DownSince = 0;
   _btn2DownSince = 0;
@@ -24,17 +23,14 @@ void OswHal::setupButtons(void) {
   _btn1UpSince = now;  // we need the +1 for the detection to begin working
   _btn2UpSince = now;
   _btn3UpSince = now;
-  
-  //Buttons (Engine)
-  for(uint8_t i = 0  ; i < 3 ; i++){
+
+  // Buttons (Engine)
+  for (uint8_t i = 0; i < 3; i++) {
     btnLastState[i] = false;
     btnIsDown[i] = false;
     btnGoneUp[i] = false;
   }
-
 }
-
-
 
 void OswHal::checkButtons(void) {
   if (digitalRead(BTN_1) == LOW && millis() - _btn1UpSince > BTN_CLICK_TIMEOUT) {
@@ -61,36 +57,32 @@ void OswHal::checkButtons(void) {
     _btn3UpSince = millis();
   }
 
-  //Buttons (Engine)
+  // Buttons (Engine)
   btnIsDown[0] = digitalRead(BTN_1) == LOW;
   btnIsDown[1] = !digitalRead(BTN_2) == LOW;
   btnIsDown[2] = !digitalRead(BTN_3) == LOW;
 
-  for(uint8_t i = 0  ; i < 3 ; i++){
+  for (uint8_t i = 0; i < 3; i++) {
     btnGoneUp[i] = btnLastState[i] == true && btnIsDown[i] == false;
     btnGoneDown[i] = btnLastState[i] == false && btnIsDown[i] == true;
-    
-    if(btnGoneDown[i]){
-      btnIsDownSince[i] = millis();  
+
+    if (btnGoneDown[i]) {
+      btnIsDownSince[i] = millis();
     }
     btnLongPress[i] = millis() > btnIsDownSince[i] + DEFAULTLAUNCHER_LONG_PRESS && btnIsDown[i] == true;
     btnLastState[i] = btnIsDown[i];
 
-    if(btnSuppressUntilUpAgain[i] == true && btnIsDown[i]){
+    if (btnSuppressUntilUpAgain[i] == true && btnIsDown[i]) {
       btnGoneUp[i] = false;
       btnGoneDown[i] = false;
-      btnIsDownSince[i] = millis();  
+      btnIsDownSince[i] = millis();
       btnLongPress[i] = false;
       btnIsDown[i] = false;
-    }else{
+    } else {
       btnSuppressUntilUpAgain[i] = false;
     }
   }
-
 }
-
-
-
 
 long OswHal::btn1Down(void) { return _btn1UpSince < _btn1DownSince ? millis() - _btn1DownSince : 0; }
 long OswHal::btn2Down(void) { return _btn2UpSince < _btn2DownSince ? millis() - _btn2DownSince : 0; }
@@ -100,14 +92,9 @@ void OswHal::clearBtn1(void) { _btn1UpSince = millis(); }
 void OswHal::clearBtn2(void) { _btn2UpSince = millis(); }
 void OswHal::clearBtn3(void) { _btn3UpSince = millis(); }
 
-//Buttons (Engine)
-bool OswHal::btnHasGoneDown(uint8_t btn){return btnGoneDown[btn];}
-bool OswHal::btnHasGoneUp(uint8_t btn){return btnGoneUp[btn];}
-bool OswHal::btnIsPhysicalDown(uint8_t btn){return btnIsDown[btn];}
-bool OswHal::btnIsLongPress(uint8_t btn){return btnLongPress[btn];}
-void OswHal::suppressButtonUntilUp(uint8_t btn){btnSuppressUntilUpAgain[btn] = true;}
-
-
-
-
-
+// Buttons (Engine)
+bool OswHal::btnHasGoneDown(uint8_t btn) { return btnGoneDown[btn]; }
+bool OswHal::btnHasGoneUp(uint8_t btn) { return btnGoneUp[btn]; }
+bool OswHal::btnIsPhysicalDown(uint8_t btn) { return btnIsDown[btn]; }
+bool OswHal::btnIsLongPress(uint8_t btn) { return btnLongPress[btn]; }
+void OswHal::suppressButtonUntilUp(uint8_t btn) { btnSuppressUntilUpAgain[btn] = true; }
