fc1549bc092b123a36c5d153b82d2ab3579a3dc3
Simon Beginn <simon@simonmicro.de>
2021-05-17 20:50:07 +0200

    Removed theme color caching

--- include/osw_ui.h
+++ include/osw_ui.h
@@ -19,22 +19,12 @@ class OswUI {
   uint16_t getWarningColor(void);
   uint16_t getDangerColor(void);
 
-  void reloadThemeColors(void);
   void resetTextColors(void);
   void setTextCursor(Button btn);
 
  private:
   static OswUI instance;
   OswHal* _hal;
-  uint16_t themeBackgroundColor = 0;
-  uint16_t themeBackgroundDimmedColor = 0;
-  uint16_t themeForegroundColor = 0;
-  uint16_t themeForegroundDimmedColor = 0;
-  uint16_t themePrimaryColor = 0;
-  uint16_t themeInfoColor = 0;
-  uint16_t themeSuccessColor = 0;
-  uint16_t themeWarningColor = 0;
-  uint16_t themeDangerColor = 0;
 };
 
 #endif
