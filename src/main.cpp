d6687f063617e88a0b0b1affa41b4652db87e873
Morthersmort <83604864+Morthersmort@users.noreply.github.com>
2021-05-19 14:21:24 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -170,19 +170,3 @@ void loop() {
 #endif
   }
 }
-
-    © 2021 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Status
-    Docs
-
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
-
-Loading complete
