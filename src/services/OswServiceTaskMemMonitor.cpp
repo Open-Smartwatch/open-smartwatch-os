7b79ebd2ab316a656e58207ea3dd721fbe74c041
Simon Beginn <simon@simonmicro.de>
2021-05-22 15:41:52 +0200

    Added some comments

--- src/services/OswServiceTaskMemMonitor.cpp
+++ src/services/OswServiceTaskMemMonitor.cpp
@@ -5,6 +5,10 @@
 
 void OswServiceTaskMemMonitor::setup(OswHal* hal) { OswServiceTask::setup(hal); }
 
+/**
+ * Updates the current high water mark for the core on which this service is running on (core 0),
+ * updates the high watermark for the heap and calls printStats() on changes
+ */
 void OswServiceTaskMemMonitor::loop(OswHal* hal) {
   unsigned core0 = uxTaskGetStackHighWaterMark(nullptr);
   unsigned high = xPortGetMinimumEverFreeHeapSize();
@@ -15,6 +19,10 @@ void OswServiceTaskMemMonitor::loop(OswHal* hal) {
   }
 }
 
+/**
+ * This should periodically be called from the loop on core 1, it updates his stack high
+ * watermarks and also calls printStats() on changes
+ */
 void OswServiceTaskMemMonitor::updateLoopTaskStats() {
   unsigned core1 = uxTaskGetStackHighWaterMark(nullptr);
   if (core1 != this->core1high) {
@@ -23,6 +31,9 @@ void OswServiceTaskMemMonitor::updateLoopTaskStats() {
   }
 }
 
+/**
+ * Send a overview regarding the current stack watermarks (core 0&1), heap watermarks and heap useage to serial
+ */
 void OswServiceTaskMemMonitor::printStats() {
   Serial.println("========= Memory Monitor =========");
   Serial.print("core 0 (high):\t");
@@ -31,7 +42,8 @@ void OswServiceTaskMemMonitor::printStats() {
   Serial.print(OswServiceManager::getInstance().workerStackSize);
   Serial.println("B");
 
-  const unsigned maxCore1 = 8192; // This value is based on https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/main.cpp
+  const unsigned maxCore1 =
+      8192;  // This value is based on https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/main.cpp
   Serial.print("core 1 (high):\t");
   Serial.print(maxCore1 - this->core1high);
   Serial.print("B of ");
@@ -49,4 +61,6 @@ void OswServiceTaskMemMonitor::printStats() {
   Serial.print("B of ");
   Serial.print(ESP.getHeapSize());
   Serial.println("B");
+
+  // TODO Maybe fetch current largest available heap size and calc "fragmentation" percentage.
 }
