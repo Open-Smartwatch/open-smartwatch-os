1bb2781efa2832bb16ce763421e1a4cd995f16ec
Simonmicro <simon@simonmicro.de>
2021-09-02 19:06:49 +0200

    Force wifi module into sleep if no on boot network is enabled

--- src/services/OswServiceTaskWiFi.cpp
+++ src/services/OswServiceTaskWiFi.cpp
@@ -18,10 +18,16 @@
 
 void OswServiceTaskWiFi::setup(OswHal* hal) {
   OswServiceTask::setup(hal);
-  this->enableWiFi();
+  this->disableStation(); // Never enable station mode after boot
   #ifdef OSW_FEATURE_WIFI_ONBOOT
-  if(OswConfigAllKeys::wifiBootEnabled.get())
+  if(OswConfigAllKeys::wifiBootEnabled.get()) {
+    this->enableWiFi();
     this->connectWiFi();
+  } else {
+  #endif
+  this->disableWiFi();
+  #ifdef OSW_FEATURE_WIFI_ONBOOT
+  }
   #endif
 }
 
