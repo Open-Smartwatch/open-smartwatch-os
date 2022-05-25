5d9f448acd5899db49bb6c451a0375dd5a7968dd
Ruffalo-sunghwan <jsh970307@daum.net>
2022-05-25 23:38:25 +0900

    Apps/tools : clean variable name

--- src/apps/tools/OswAppStepStats.cpp
+++ src/apps/tools/OswAppStepStats.cpp
@@ -10,28 +10,28 @@
 
 void OswAppStepStats::drawChart() {
   OswHal* hal = OswHal::getInstance();
-  uint8_t frame_h = 55;
+  uint8_t chartStickHeight = 55;
 
-  uint8_t dt = 20;
-  uint16_t max_goal = OswConfigAllKeys::stepsPerDay.get();
+  uint8_t interval = 20;
+  uint16_t goalValue = OswConfigAllKeys::stepsPerDay.get();
 
   uint32_t weekDay = 0;
   uint32_t dayOfMonth = 0;
   hal->getLocalDate(&dayOfMonth, &weekDay);
-  
-  for (uint8_t i = 0; i < 7; i++) {
-    uint32_t s = hal->environment->getStepsOnDay(i);  // virtual step simulation
-    uint16_t boxHeight = ((float)(s > max_goal ? max_goal : s) / max_goal) * frame_h;
 
-    uint16_t barColor = OswConfigAllKeys::stepsPerDay.get() <= s ? ui->getSuccessColor() : ui->getPrimaryColor();
+  for (uint8_t index = 0; index < 7; index++) {
+    uint32_t weekDayStep = hal->environment->getStepsOnDay(index);  // virtual step simulation
+    uint16_t chartStickValue = ((float)(weekDayStep > goalValue ? goalValue : weekDayStep) / goalValue) * chartStickHeight;
 
-    boxHeight = boxHeight < 2 ? 0 : boxHeight;
+    uint16_t barColor = OswConfigAllKeys::stepsPerDay.get() <= weekDayStep ? ui->getSuccessColor() : ui->getPrimaryColor();
+
+    chartStickValue = chartStickValue < 2 ? 0 : chartStickValue;
     // step bars
 
-    if (i == weekDay) {
-      hal->gfx()->drawThickTick(180 - i * dt, 150 - 3, 0, frame_h, 0, 5, ui->getForegroundColor());
+    if (index == weekDay) {
+      hal->gfx()->drawThickTick(180 - index * interval, 150 - 3, 0, chartStickHeight, 0, 5, ui->getForegroundColor());
     }
-    hal->gfx()->drawThickTick(180 - i * dt, 150 - 3, 0, boxHeight, 0, 3, barColor, true);
+    hal->gfx()->drawThickTick(180 - index * interval, 150 - 3, 0, chartStickValue, 0, 3, barColor, true);
   }
 }
 
