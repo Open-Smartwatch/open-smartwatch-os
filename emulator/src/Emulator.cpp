0b45dc4f3de378b3ce1365649eabcb23426ed853
simonmicro <simon@simonmicro.de>
2022-06-11 18:43:58 +0200

    Implemented flickery demo window

--- emulator/src/Emulator.cpp
+++ emulator/src/Emulator.cpp
@@ -2,44 +2,58 @@
 #include "imgui_impl_sdl.h"
 #include "imgui_impl_sdlrenderer.h"
 
-#include "../include/Display.h"
+#include "../../include/config_defaults.h" // For the display size
 
+#include "../include/Display.h"
 #include "../include/Emulator.hpp"
 
 OswEmulator* OswEmulator::instance = nullptr;
 
 OswEmulator::OswEmulator() {
-    this->mainWindow = SDL_CreateWindow("OSW-OS Emulator Display", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 256, 256, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    this->mainWindow = SDL_CreateWindow("OSW-OS Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DISP_W, DISP_H, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
     assert(this->mainWindow && "Never fail window creation");
     this->mainRenderer = SDL_CreateRenderer(this->mainWindow, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
     assert(this->mainRenderer && "Never fail renderer creation");
-    fakeDisplayInstance = std::make_unique<FakeDisplay>(256, 256, this->mainWindow, this->mainRenderer);
+    fakeDisplayInstance = std::make_unique<FakeDisplay>(DISP_W, DISP_H, this->mainWindow, this->mainRenderer);
 
-IMGUI_CHECKVERSION();
-ImGui::CreateContext();
-ImGuiIO& io = ImGui::GetIO(); (void)io;
-//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableSetMousePos;
 
-ImGui::StyleColorsDark();
-ImGui_ImplSDL2_InitForSDLRenderer(this->mainWindow, this->mainRenderer);
-ImGui_ImplSDLRenderer_Init(this->mainRenderer);
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL2_InitForSDLRenderer(this->mainWindow, this->mainRenderer);
+    ImGui_ImplSDLRenderer_Init(this->mainRenderer);
 
     this->enterSleep(true); // The intial state of the watch!
 }
 
 OswEmulator::~OswEmulator() {
+    ImGui_ImplSDLRenderer_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
     SDL_DestroyWindow(this->mainWindow);
 }
 
 void OswEmulator::run() {
 
+    bool show_demo_window = true;
     while(this->running) {
+        SDL_RenderClear(this->mainRenderer);
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
             if(event.type == SDL_QUIT)
                 this->running = false;
         }
+
+        ImGui_ImplSDLRenderer_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+        ImGui::ShowDemoWindow(&show_demo_window);
+        ImGui::Render();
 
         if(this->deepSleeped) {
             setup();
@@ -52,6 +66,7 @@ void OswEmulator::run() {
         }
 
         // Update the window now with the content of the display
+        ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
         SDL_RenderPresent(this->mainRenderer);
     }
 }
