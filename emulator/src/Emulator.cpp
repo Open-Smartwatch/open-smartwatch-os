46b1ad52b266a858882d32d734f8afe19761c4ee
simonmicro <simon@simonmicro.de>
2022-06-12 22:23:51 +0200

    Minor cleanup

--- emulator/src/Emulator.cpp
+++ emulator/src/Emulator.cpp
@@ -10,18 +10,19 @@
 OswEmulator* OswEmulator::instance = nullptr;
 
 OswEmulator::OswEmulator() {
+    // Init the SDL window and renderer
     this->mainWindow = SDL_CreateWindow("OSW-OS Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DISP_W, DISP_H, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
     assert(this->mainWindow && "Never fail window creation");
     this->mainRenderer = SDL_CreateRenderer(this->mainWindow, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
     assert(this->mainRenderer && "Never fail renderer creation");
     fakeDisplayInstance = std::make_unique<FakeDisplay>(DISP_W, DISP_H, this->mainWindow, this->mainRenderer);
 
+    // Create ImGUI context and initialize
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableSetMousePos;
-
     ImGui::StyleColorsDark();
     ImGui_ImplSDL2_InitForSDLRenderer(this->mainWindow, this->mainRenderer);
     ImGui_ImplSDLRenderer_Init(this->mainRenderer);
@@ -42,6 +43,7 @@ void OswEmulator::run() {
     bool show_demo_window = true;
     while(this->running) {
         SDL_RenderClear(this->mainRenderer);
+
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             ImGui_ImplSDL2_ProcessEvent(&event);
@@ -49,24 +51,31 @@ void OswEmulator::run() {
                 this->running = false;
         }
 
+        // Prepare ImGUI for the next frame
         ImGui_ImplSDLRenderer_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
-        ImGui::ShowDemoWindow(&show_demo_window);
+        if(show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
         ImGui::Render();
 
+        // Revive system after deep sleep as needed
         if(this->deepSleeped) {
             setup();
             this->deepSleeped = false;
         }
+
+        // Next OS step
         try {
             loop();
         } catch(EmulatorSleep& e) {
             // Ignore it :P
         }
 
-        // Update the window now with the content of the display
+        // Draw ImGUI content
         ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
+
+        // Update the window now with the content of the display
         SDL_RenderPresent(this->mainRenderer);
     }
 }
