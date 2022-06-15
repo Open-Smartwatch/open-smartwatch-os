2045dc5ca6026d85ef368eb4afd6cb80eb56fdc4
GitHub Action <action@github.com>
2022-06-15 10:30:11 +0000

    Applied formatting

--- include/apps/watchfaces/OswAppWatchfaceDual.h
+++ include/apps/watchfaces/OswAppWatchfaceDual.h
@@ -14,7 +14,7 @@ class OswAppWatchfaceDual : public OswApp {
     virtual void loop() override;
     virtual void stop() override;
     ~OswAppWatchfaceDual() {};
-    
+
     //for dual-time
     static void drawProgressBar(OswUI* ui,uint8_t cx, uint8_t cy, uint8_t jump, uint8_t length, uint8_t value,float angle, uint8_t radius, uint16_t color, uint8_t* goal=nullptr);
     void drawAnimSec();
