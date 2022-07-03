6f9e2ee924d3090d01b28945fa24d37f9be85a7e
RuffaloLavoisier <jsh970307@daum.net>
2022-07-03 12:35:01 +0900

    OswServiceTasks : update OSW_FEATURE_WIFI

--- include/services/OswServiceTasks.h
+++ include/services/OswServiceTasks.h
@@ -3,16 +3,19 @@
 class OswServiceTaskBLECompanion;
 class OswServiceTaskExample;
 class OswServiceTaskMemMonitor;
+#ifdef OSW_FEATURE_WIFI
 class OswServiceTaskWiFi;
 class OswServiceTaskWebserver;
-
+#endif
 namespace OswServiceAllTasks {
 #if SERVICE_BLE_COMPANION == 1
 extern OswServiceTaskBLECompanion bleCompanion;
 #endif
 //extern OswServiceTaskExample example;
+#ifdef OSW_FEATURE_WIFI
 extern OswServiceTaskWiFi wifi;
 extern OswServiceTaskWebserver webserver;
+#endif
 extern OswServiceTaskMemMonitor memory;
 }
 
