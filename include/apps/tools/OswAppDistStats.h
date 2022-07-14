dc6387a24c2ee69aac964e6849b0734881875d76
RuffaloLavoisier <RuffaloLavoisier@gmail.com>
2022-07-14 21:51:48 +0900

    Dist-stats : add Distance stats

---
+++ include/apps/tools/OswAppDistStats.h
@@ -0,0 +1,26 @@
+#ifdef OSW_FEATURE_STATS_STEPS
+
+#pragma once
+
+#include <osw_hal.h>
+#include <osw_ui.h>
+#include "osw_app.h"
+
+class OswAppDistStats : public OswApp {
+  public:
+    OswAppDistStats(void) {
+        ui = OswUI::getInstance();
+    };
+    virtual void setup() override;
+    virtual void loop() override;
+    virtual void stop() override;
+    ~OswAppDistStats() {};
+
+  private:
+    void showStickChart();
+    void drawChart();
+    OswUI* ui;
+    uint32_t cursorPos; // WeekDay position
+};
+
+#endif
