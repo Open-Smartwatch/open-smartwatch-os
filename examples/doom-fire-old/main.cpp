414e2f122d5441d5159d3b8281d9d7767ea8cfd4
RuffaloLavoisier <RuffaloLavoisier@gmail.com>
2022-07-20 09:43:05 +0900

    Anim : refactoring doom-fire class

--- examples/doom-fire-old/main.cpp
+++ examples/doom-fire-old/main.cpp
@@ -5,31 +5,30 @@
 
 #include "../../FakeArduino.h"
 #include "../../FakeArduinoWindowSDL.h"
-#include "../../anim_doom_fire.h"
+#include "../../anim_doom_fire_old.h"
 #include "../../gfx_util.h"
 using namespace std;
 
 #define BUF_W 240
 #define BUF_H 240
+uint8_t** firePixels = new uint8_t*[BUF_H];
+Graphics2D gfx2d(BUF_W, BUF_H,120);
 
-Graphics2DPrint gfx2d(BUF_W, BUF_H, 120);
-AnimDoomFire d;
 class FireWindow : public SDLWindowRGB565 {
  public:
   FireWindow(Graphics2D* gfx2d, int w, int h) : SDLWindowRGB565(gfx2d, w, h) {}
   void setup() {
-
+    // setup array rows
+    for (int i = 0; i < BUF_H; i++) {
+      firePixels[i] = new uint8_t[BUF_W];
+    }
+    setupFire(firePixels, BUF_W, BUF_H);
   }
 
   void loop() {
-    Graphics2DPrint* gfx = &gfx2d;
-
-    gfx->fill(rgb565(0, 0, 0));
-
-    // render animation
-    d.loop(gfx);
-
     delay(1000 / 30);
+    calcFire(firePixels, BUF_W, BUF_H);
+    mapFire(firePixels, BUF_W, BUF_H, &gfx2d, 0, 0);
   }
 };
 
