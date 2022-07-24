99e300c289ccb24c1694712b5b42d451826308b1
RuffaloLavoisier <RuffaloLavoisier@gmail.com>
2022-07-24 11:58:42 +0900

    Libraries : clean header

--- include/osw_app.h
+++ include/osw_app.h
@@ -1,13 +1,8 @@
-#ifndef OSW_APP_H
-#define OSW_APP_H
-
+#pragma once
 #include <osw_hal.h>
-
 class OswApp {
   public:
     virtual void setup() = 0;
     virtual void loop() = 0;
     virtual void stop() = 0;
 };
-
-#endif
