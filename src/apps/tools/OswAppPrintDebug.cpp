e91aaf41d54cf0c629b12763ba41884301457dae
RuffaloLavoisier <RuffaloLavoisier@gmail.com>
2022-07-27 18:15:28 +0900

    Apply slice feature

--- src/apps/tools/OswAppPrintDebug.cpp
+++ src/apps/tools/OswAppPrintDebug.cpp
@@ -75,7 +75,8 @@ void OswAppPrintDebug::loop() {
     wifiDisabled = !OswServiceAllTasks::wifi.isEnabled();
 #endif
     printStatus("Battery (Analog)", (wifiDisabled ? String(hal->getBatteryRaw()) : String("WiFi active!")).c_str());
-    printStatus("Hash", (String(GIT_COMMIT_HASH) + " (" + String(GIT_BRANCH_NAME) + ")").c_str());
+    char branchName[] = GIT_BRANCH_NAME;
+    printStatus("Hash", (String(GIT_COMMIT_HASH) + " (" + hal->gfx()->printSlice(branchName, 10,  true) + ".." + ")").c_str());
     printStatus("Platform", String(PIO_ENV_NAME).c_str());
     printStatus("Compiled", (String(__DATE__) + " " + String(__TIME__)).c_str());
 
