224da51d434b6ee1fc25664f54c489dd368703c1
simonmicro <simon@simonmicro.de>
2022-08-05 21:33:38 +0200

    Using real-value defines

--- emulator/include/Defines.h
+++ emulator/include/Defines.h
@@ -1,6 +1,2 @@
-#define GIT_COMMIT_HASH "0000000"
-#define GIT_BRANCH_NAME "null"
-#define PIO_ENV_NAME "VIRTUAL"
-
 #include <iostream>
 #define OSW_EMULATOR_THIS_IS_NOT_IMPLEMENTED std::cerr << "FIXME: " << __FILE__ << ":" << __LINE__ << "->" << __FUNCTION__ << "() Not implemented!" << std::endl;
