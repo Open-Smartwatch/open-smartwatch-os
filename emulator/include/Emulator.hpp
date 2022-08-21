6634e9a743c88d42f89823824bc17700a1cb4264
Ruffalo Lavoisier <RuffaloLavoisier@gmail.com>
2022-08-21 13:47:48 +0000

    Emulator: add web-interface key
    - dropdown, checkbox, input int and float, color section

--- emulator/include/Emulator.hpp
+++ emulator/include/Emulator.hpp
@@ -4,6 +4,12 @@
 #include <atomic>
 #include <SDL2/SDL.h>
 
+#include "gfx_2d.h"
+#include "gfx_util.h"
+
+#include "config_defaults.h"
+#include "config.h"
+
 void setup();
 void loop();
 
@@ -19,6 +25,7 @@ public:
     ~OswEmulator();
 
     void run();
+    void drawWatchOverlay();
     void exit();
 
     void setButton(unsigned id, bool state);
@@ -35,6 +42,7 @@ private:
         lightSpleep,
         deepSleep
     };
+    Graphics2D* graphics2d;
 
     SDL_Window* mainWindow = nullptr; // Do not delete() this, this is done by SDL2
     SDL_Renderer* mainRenderer = nullptr;
@@ -47,9 +55,41 @@ private:
     bool wakeUpNow = false;
 
     // Web-interface
+
+    // CheckBox section
     bool checkBoxTimeFormat = false;
     bool checkBoxSettingDisplayOverlays = false;
     bool checkBoxSettingDisplayOverlaysOnWatchScreen = false;
+    bool checkBoxRaiseToWakeEnabled = false;
+    bool checkBoxButtonToWakeEnabled = false;
+    bool checkBoxTapToWakeEnabled = false;
+    bool checkBoxLightSleepEnabled = false;
+    bool checkBoxStepsHistoryClear = false;
+
+    // Color section
+    float colorThemeBackgroundColor[3] = {0/ 255.0F, 0/ 255.0F, 0/ 255.0F};
+    float colorThemeBackgroundDimmedColor[3] = {64/ 255.0F, 64/ 255.0F, 64/ 255.0F};
+    float colorThemeForegroundColor[3] = {255/ 255.0F, 255/ 255.0F, 255/ 255.0F};
+    float colorThemeForegroundDimmedColor[3] = {122/ 255.0F, 122/ 255.0F, 122/ 255.0F};
+    float colorThemePrimaryColor[3] = {0/ 255.0F, 209/ 255.0F, 178/ 255.0F};
+    float colorThemeInfoColor[3] = {32/ 255.0F, 156/ 255.0F, 238/ 255.0F};
+    float colorThemeSuccessColor[3] = {72/ 255.0F, 199/ 255.0F, 116/ 255.0F};
+    float colorThemeWarningColor[3] = {255/ 255.0F, 221/ 255.0F, 87/ 255.0F};
+    float colorThemeDangerColor[3] = {255 / 255.0F, 56 / 255.0F, 96 / 255.0F};
+
+    // Dropdown section
+    // int comboDateFormat = 0;
+    int comboSettingDisplayDefaultWatchface = 0;
+
+    // input editor (int) section
+    int inputintSettingDisplayBrightness = DISPLAY_BRIGHTNESS;
+    int inputintSettingDisplayTimeout = DISPLAY_TIMEOUT;
+    int inputintRaiseToWakeSensitivity = WAKE_FROM_RAISE_SENSITIVITY;
+    int inputintTimeZone = CONFIG_TIMEZONE;
+    int inputintStepsPerDay = STEPS_PER_DAY;
+
+    // input editor (float) section
+    float inputfloatDaylightOffset = CONFIG_DAYLIGHTOFFSET;
 
     const unsigned reduceFlickerFrames = 600;
 
