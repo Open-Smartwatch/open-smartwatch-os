98cf705ef5dfaa18e1c0064093c8e2848c8419d1
David Schramm <david.schramm@live.com>
2022-09-03 14:15:46 +0200

    minor improvements

--- src/apps/watchfaces/OswAppWatchfaceMonotimer.cpp
+++ src/apps/watchfaces/OswAppWatchfaceMonotimer.cpp
@@ -16,17 +16,40 @@
 #include "./apps/_experiments/gif_player.h"
 #endif
 
+/**
+ * @brief Draw N shifted ticks around the clock.
+ *
+ * @param cx center x axis
+ * @param cy center y axis
+ * @param r1 radius from the begin of the tick.
+ * @param r2 radius from the end of the tick.
+ * @param nTicks number of ticks to draw
+ * @param shift amount of degrees the ticks are shifted clockwise from the top (0°)
+ * @param color color code
+ */
 void OswAppWatchfaceMonotimer::drawNShiftedTicks(Graphics2D *gfx, uint8_t cx, uint8_t cy, uint8_t r1, uint8_t r2, uint8_t nTicks, float shift, uint16_t color) {
     const float deltaAngle = 360.0 / nTicks;
-    for(uint16_t i=0; i<nTicks; ++i) {
+    for (uint16_t i=0; i<nTicks; ++i) {
         gfx->drawTick(cx, cy, r1, r2, (i * deltaAngle) + shift, color);
     }
 }
 
+/**
+ * @brief Draw N shifted ticks around the clock; skip every m-th.
+ *
+ * @param cx center x axis
+ * @param cy center y axis
+ * @param r1 radius from the begin of the tick.
+ * @param r2 radius from the end of the tick.
+ * @param nTicks number of ticks to draw
+ * @param shift amount of degrees the ticks are shifted clockwise from the top (0°)
+ * @param m do not draw every m-th tick
+ * @param color color code
+ */
 void OswAppWatchfaceMonotimer::drawNShiftedMaskedTicks(Graphics2D *gfx, uint8_t cx, uint8_t cy, uint8_t r1, uint8_t r2, uint8_t nTicks, float shift, uint16_t m, uint16_t color) {
     const float deltaAngle = 360.0 / nTicks;
-    for(uint16_t i=0; i<nTicks; ++i) {
-        if(i%m) {
+    for (uint16_t i=0; i<nTicks; ++i) {
+        if (i % m) {
             gfx->drawTick(cx, cy, r1, r2, (i * deltaAngle) + shift, color);
         }
     }
@@ -63,7 +86,7 @@ void OswAppWatchfaceMonotimer::drawWatch() {
         110, 30 	// 12
     };
 
-    for(uint8_t i=0; i<12; ++i) {
+    for (uint8_t i=0; i<12; ++i) {
         hal->gfx()->setTextCursor(positions[i<<1], positions[(i<<1)|1]);
         hal->gfx()->printDecimal(i+1, 2);
     }
@@ -77,9 +100,7 @@ void OswAppWatchfaceMonotimer::drawWatch() {
     hal->gfx()->setTextCursor(120, 135);
     hal->gfx()->setTextColor(steps>stepsTarget?ui->getSuccessColor():ui->getInfoColor());
 
-    char buf[16];
-    snprintf(buf, 16, "%u", steps);
-    hal->gfx()->print(buf);
+    hal->gfx()->print(steps);
 #endif
 
     // ticks
@@ -88,7 +109,7 @@ void OswAppWatchfaceMonotimer::drawWatch() {
     uint32_t hour = 0;
     hal->getLocalTime(&hour, &minute, &second);
 
-    if(OswConfigAllKeys::settingDisplayDualHourTick.get()) {
+    if (OswConfigAllKeys::settingDisplayDualHourTick.get()) {
         uint32_t dualSecond = 0;
         uint32_t dualMinute = 0;
         uint32_t dualHour = 0;
