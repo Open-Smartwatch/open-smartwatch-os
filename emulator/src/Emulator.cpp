dbe4f69c9bdc78bbce4b376150df54d360ef8bc9
Ruffalo Lavoisier <RuffaloLavoisier@gmail.com>
2022-09-06 21:23:59 +0900

    Set emulator window title

--- emulator/src/Emulator.cpp
+++ emulator/src/Emulator.cpp
@@ -17,7 +17,7 @@ OswEmulator* OswEmulator::instance = nullptr;
 
 OswEmulator::OswEmulator() {
     // Init the SDL window and renderer
-    this->mainWindow = SDL_CreateWindow("smartwatch virtual device", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DISP_W + this->guiPadding + this->guiWidth + this->guiPadding, DISP_H, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    this->mainWindow = SDL_CreateWindow("OSW-OS Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DISP_W + this->guiPadding + this->guiWidth + this->guiPadding, DISP_H, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
     assert(this->mainWindow && "Never fail window creation");
     this->mainRenderer = SDL_CreateRenderer(this->mainWindow, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
     assert(this->mainRenderer && "Never fail renderer creation");
