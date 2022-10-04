050f23392d7e063962bd09b621a637e9cf963c70
ich42 <80181603+ich42@users.noreply.github.com>
2022-10-04 13:06:32 +0900

    config: add calculator and flashlight

--- include/config_defaults.h
+++ include/config_defaults.h
@@ -211,6 +211,12 @@
 #ifndef GAME_BRICK_BREAKER
 #define GAME_BRICK_BREAKER 0
 #endif
+#ifndef TOOL_FLASHLIGHT
+#define TOOL_FLASHLIGHT 1
+#endif
+#ifndef TOOL_CALCULATOR
+#define TOOL_CALCULATOR 1
+#endif
 #ifndef TOOL_STOPWATCH
 #define TOOL_STOPWATCH 1
 #endif
