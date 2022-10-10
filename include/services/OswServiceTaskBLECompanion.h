79a29e026e183421a4c218169d809378c493c939
Eli Roberts <my310toyuma@gmail.com>
2022-10-10 15:47:49 -0400

    just redoing ble change

---
+++ include/services/OswServiceTaskBLECompanion.h
@@ -0,0 +1,49 @@
+#ifndef OSW_SERVICE_COMPANION_H
+#define OSW_SERVICE_COMPANION_H
+
+#ifndef OSW_EMULATOR
+#include <BLECharacteristic.h>
+#include <BLEDevice.h>
+#include <osw_hal.h>
+#include <functional>
+#include <string>
+
+#include "osw_service.h"
+
+#define NOTIFICATION_SERVICE_UID "30412632-6339-46e3-ad9e-bcfa9a766854"
+
+#define NOTIFICATION_BRDCST_CHAR "23dac1dc-ca00-47ed-a5fa-e3b9da959685"
+
+struct NotificationDetails {
+    unsigned int uid;
+    std::string app;
+    std::string contents;
+};
+
+class OswServiceTaskBLECompanion : public OswServiceTask {
+  public:
+    OswServiceTaskBLECompanion(void) {};
+    virtual void setup() override;
+    virtual void loop() override;
+    virtual void stop() override;
+    ~OswServiceTaskBLECompanion() {};
+    bool isBLEEnabled();
+
+    void setNotificationCallback(std::function<void(NotificationDetails)> cb);
+    void startAdvertising();
+    void stopAdvertising();
+    bool m_enableBLEClient = false;
+
+  private:
+    BLEServer* bleServer = NULL;
+    BLEService* notificationService = NULL;
+    BLECharacteristic* notificationChar = NULL;
+
+    std::function<void(NotificationDetails)> notificationCallback;
+
+    friend class NotificationCallback;
+    friend class OswAppBLEMediaCtrl;
+};
+
+#endif
+#endif
---
+++ src/services/OswServiceTaskBLECompanion.cpp
@@ -0,0 +1,104 @@
+#ifndef OSW_EMULATOR
+#include "./services/OswServiceTaskBLECompanion.h"
+#include "./apps/tools/OswAppBLEMediaCtrl.h"
+#include "osw_hal.h"
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLECharacteristic.h>
+#include <ArduinoJson.h>
+
+#include <string.h>
+#include "config_defaults.h"
+
+#ifdef OSW_FEATURE_WIFI
+#if SERVICE_BLE_COMPANION == 1
+#error "The RAM on all current OSW models is not big enough to hold both WiFi AND Bluetooth stacks during runtime. This WILL lead to a crash. Please only use one of these features simultaneously!"
+#endif
+#endif
+
+class NotificationCallback: public BLECharacteristicCallbacks {
+  public:
+    NotificationCallback(OswServiceTaskBLECompanion* comp) {
+        companion = comp;
+    }
+
+    virtual ~NotificationCallback() {};
+
+    virtual void onRead(BLECharacteristic* pCharacteristic) {};
+    virtual void onWrite(BLECharacteristic* pCharacteristic) {
+        //Parse message as JSON object
+        Serial.println(pCharacteristic->getValue().c_str());
+        DynamicJsonDocument doc(5000); //ArduinoJSON no longer allows Dynamic allocation to grow
+
+        deserializeJson(doc, pCharacteristic->getValue());
+
+        //Pull values from JSON
+        const char* app = "", *contents = "";
+        unsigned int uid = 0;
+
+        if (doc.containsKey("app"))
+            app = doc["app"];
+
+        if (doc.containsKey("contents"))
+            contents = doc["contents"];
+
+        if (doc.containsKey("uid"))
+            uid = doc["uid"];
+
+        NotificationDetails details {uid, app, contents};
+
+        // Notify our client about the new notification
+        if (companion->notificationCallback) {
+            companion->notificationCallback(details);
+        }
+    };
+
+    virtual void onNotify(BLECharacteristic* pCharacteristic) {};
+    virtual void onStatus(BLECharacteristic* pCharacteristic, Status s, uint32_t code) {};
+
+  private:
+    OswServiceTaskBLECompanion* companion;
+};
+
+void OswServiceTaskBLECompanion::setup() {
+    OswServiceTask::setup();
+    this->m_enableBLEClient = true;
+    BLEDevice::init(BLE_DEVICE_NAME);
+    bleServer = BLEDevice::createServer();
+    notificationService = bleServer->createService(NOTIFICATION_SERVICE_UID);
+    Serial.println("this is BLE SERVICE UID -> " + String(NOTIFICATION_SERVICE_UID));   //* updated sept 14 2022
+
+    notificationChar = notificationService->createCharacteristic(NOTIFICATION_BRDCST_CHAR, BLECharacteristic::PROPERTY_WRITE);
+    notificationChar->setCallbacks(new NotificationCallback(this));
+    if(OswServiceTaskBLECompanion::isBLEEnabled()) Serial.println("BLE is enabled");
+
+    notificationService->start();
+
+    bleServer->getAdvertising()->addServiceUUID(notificationService->getUUID());
+
+    // added Sept 14 2022 
+    bleServer->startAdvertising();
+}
+
+void OswServiceTaskBLECompanion::startAdvertising() {
+    bleServer->startAdvertising();
+}
+
+void OswServiceTaskBLECompanion::stopAdvertising() {
+    bleServer->getAdvertising()->stop();
+}
+
+void OswServiceTaskBLECompanion::setNotificationCallback(std::function<void(NotificationDetails)> cb) {
+    notificationCallback = cb;
+}
+
+void OswServiceTaskBLECompanion::loop() {
+
+}
+
+void OswServiceTaskBLECompanion::stop() {
+    OswServiceTask::stop();
+}
+#endif
