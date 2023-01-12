5164d30fda712fe51af30767da555d6f665e7885
akmal-threepointsix <akmal.mukh@proton.me>
2023-01-12 21:10:52 +0100

    Fix locales

--- include/locales/de-DE.h
+++ include/locales/de-DE.h
@@ -12,6 +12,13 @@
 #define LANG_CANCEL "Abbrechen"
 #define LANG_SAVE "Speichern"
 
+// App: Alarm
+#define LANG_ALARM "Alarm"
+#define LANG_NEXT "Next"
+#define LANG_DAILY "Daily"
+#define LANG_WEEKDAYS "Weekdays"
+#define LANG_WEEKENDS "Weekends"
+
 // App: BLE media control
 #define LANG_BMC_VOLUME "Lautst."
 #define LANG_BMC_CONNECTING "Verbinden..."
@@ -54,5 +61,14 @@
 #define LANG_FRIDAY "Freitag"
 #define LANG_SATURDAY "Samstag"
 #define LANG_SUNDAY "Sonntag"
+
+// Weekdays (short)
+#define LANG_SUN "Sun"
+#define LANG_MON "Mon"
+#define LANG_TUE "Tue"
+#define LANG_WED "Wed"
+#define LANG_THU "Thu"
+#define LANG_FRI "Fri"
+#define LANG_SAT "Sat"
 
 #endif
