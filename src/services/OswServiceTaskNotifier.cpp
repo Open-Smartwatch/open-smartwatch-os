f54e821c6ab97d1c25a711c58985800268fa41a2
cutieskye <68032402+cutieskye@users.noreply.github.com>
2023-01-17 19:25:26 +0000

    Improve performance with move semantics

--- src/services/OswServiceTaskNotifier.cpp
+++ src/services/OswServiceTaskNotifier.cpp
@@ -5,7 +5,7 @@ unsigned Notification::count = 0;
 NotificationData OswServiceTaskNotifier::createNotification(std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds> timeToFire, std::string publisher,
                                                             std::string message, std::array<bool, 7> daysOfWeek, bool isPersistent) {
     const std::lock_guard<std::mutex> lock{mutlimapMutex};
-    auto notification = Notification{publisher, message, daysOfWeek, isPersistent};
+    auto notification = Notification{std::move(publisher), std::move(message), std::move(daysOfWeek), isPersistent};
     auto pair = std::make_pair(timeToFire, notification);
     scheduler.insert(pair);
     return pair;
@@ -40,7 +40,7 @@ NotificationData OswServiceTaskNotifier::createNotification(int hours, int minut
                                                             std::string message, std::array<bool, 7> daysOfWeek, bool isPersistent) {
     const std::lock_guard<std::mutex> lock{mutlimapMutex};
     std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds> timeToFire{};
-    auto notification = Notification{publisher, message, daysOfWeek, isPersistent};
+    auto notification = Notification{std::move(publisher), std::move(message), std::move(daysOfWeek), isPersistent};
     if (std::any_of(daysOfWeek.begin(), daysOfWeek.end(), [](auto x) {
             return x;
         })) {
