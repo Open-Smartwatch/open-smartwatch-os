08c6cddf1c26643da23d15b073c99f2baedf5571
simonmicro <simon@simonmicro.de>
2023-04-19 17:06:49 +0200

    Removed some itendation

--- .github/getWorkflowMatrix.py
+++ .github/getWorkflowMatrix.py
@@ -35,26 +35,27 @@ def get(info: GettableInfo):
     elif info == GettableInfo.FLAGS:
         with open('docs/firmware/osw_os.md') as f:
             for line in f:
-                if match := re.match(r'^`(.+)` \| .+ \| (.+)$', line):
-                    feature = match.group(1).strip()
-                    featureRequirements = match.group(2).strip()
-                    required = [feature] + [req.strip('`,').strip() for req in featureRequirements.split(' ')]
-                    try:
-                        required.remove('-') # remove the "no flags" flag
-                    except ValueError:
-                        pass # no "no flags" flag
-                    for flag in required:
-                        if re.match(r'^[\-|A-Z0-9_]+$', flag) is None:
-                            # We drop any requirement that is not a proper flag
-                            break
-                    else:
-                        returnArray.add(str('|'.join(required)))
+                match = re.match(r'^`(.+)` \| .+ \| (.+)$', line)
+                if match is None:
+                    continue
+                feature = match.group(1).strip()
+                featureRequirements = match.group(2).strip()
+                required = [feature] + [req.strip('`,').strip() for req in featureRequirements.split(' ')]
+                try:
+                    required.remove('-') # remove the "no flags" flag
+                except ValueError:
+                    pass # no "no flags" flag
+                for flag in required:
+                    if re.match(r'^[\-|A-Z0-9_]+$', flag) is None:
+                        # We drop any requirement that is not a proper flag
+                        break
+                else:
+                    returnArray.add(str('|'.join(required)))
     elif info == GettableInfo.MODELS:
         with open('platformio.ini') as f:
             for line in f:
                 match = re.match(r'^\[env:(.+)\]$', line)
-                if match is None:
-                    continue
+                if match is None: continue
                 env = match.group(1)
                 if env.startswith('GPS'):
                     # GPS is NOT build anymore by default
