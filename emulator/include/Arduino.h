4f431d85309cc823d9e7d5555dca6db3f64e8555
simonmicro <simon@simonmicro.de>
2023-09-21 15:06:17 +0200

    Fixed broken min/max implementations

--- emulator/include/Arduino.h
+++ emulator/include/Arduino.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "DataTypes.h"
 #include "Defines.h"
 
@@ -5,5 +7,13 @@ unsigned long millis();
 long random(int howbig);
 long random(int howsmall, int howbig);
 void delay(long millis);
-int32_t min(int32_t a, int32_t b);
-int32_t max(int32_t a, int32_t b);
+
+// Following are just forwards to the their std-equivalents, specific implementations will confuse the compiler (and cause e.g. std::min != min != what-it-should-be problems)
+template<typename T>
+inline T min(T a, T b) {
+    return std::min(a, b);
+}
+template<typename T>
+inline T max(T a, T b) {
+    return std::max(a, b);
+}
