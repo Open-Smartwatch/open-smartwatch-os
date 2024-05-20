830d3054d2076de19aab88a0a9ef2dfa9ebe6c94
simonmicro <simon@simonmicro.de>
2024-05-20 21:03:32 +0200

    Added missing include
    Signed-off-by: simonmicro <simon@simonmicro.de>

--- src/services/OswServiceTaskConsole.cpp
+++ src/services/OswServiceTaskConsole.cpp
@@ -2,6 +2,7 @@
 #include "osw_hal.h"
 #include <services/OswServiceTasks.h>
 #include <services/OswServiceTaskBLEServer.h>
+#include <services/NotifierClient.h>
 
 void OswServiceTaskConsole::setup() {
     OswServiceTask::setup();
